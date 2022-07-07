d8bcc7a000299f75665ee5b1273b3ee33f906345
Talita Saez <tltsaez@gmail.com>
2022-07-07 13:57:05 -0300

    resolucao exercicio 6.1 desafio 3 e 4

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,3 +1,26 @@
-function subimeter(event) {
+function handleSubmit(event) {
     event.preventDefault();
   }
+
+  window.onload = function () {
+    const submitBtn = document.querySelector('#submeter');
+    submitBtn.addEventListener('click', handleSubmit);
+  };
+
+  function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  }
+
+  window.onload = function () {
+    const clearBtn = document.querySelector('#limpar');
+    clearBtn.addEventListener('click', clearFields);
+    const submitBtn = document.querySelector('#submeter');
+    submitBtn.addEventListener('click', handleSubmit);
+  };
