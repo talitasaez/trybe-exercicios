2465c3b6762b3e44940886ba6daf330bf6e86fb4
Talita Saez <tltsaez@gmail.com>
2022-07-15 16:18:28 -0300

    modificações arquivos js

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -0,0 +1,125 @@
+const sum = (value1, value2) => value1 + value2;
+
+console.log(sum(2, 3));
+
+const sum = (value1, value2) => value1 + value2;
+
+console.log(sum(2, '3'));
+
+const sum = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      return 'Os valores devem ser numéricos';
+    }
+    return value1 + value2;
+  };
+  
+  console.log(sum(2, '3'));
+
+  const sum = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+    return value1 + value2;
+  };
+  
+  console.log(sum(2, '3'));
+
+  const verifyIsNumber = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+  };
+  
+  const sum = (value1, value2) => {
+    try {
+      verifyIsNumber(value1, value2);
+      return value1 + value2;
+    } catch (error) {
+      return error.message;
+    }
+  };
+  
+  console.log(sum(2, '3'));
+
+   //objeto assign 
+  // A função recebe um número qualquer de parâmetros. Todos são agregados como valores para adicionar ao objeto de destino!
+
+Object.assign(destino, objeto1);
+Object.assign(destino, objeto1, objeto2);
+Object.assign(destino, objeto1, objeto2, objeto3, objeto4);
+
+const person = {
+    name: 'Alberto',
+    lastName: 'Gomes',
+    age: 20,
+  };
+  
+  const info = {
+    age: 23,
+    job: 'engenheiro',
+  };
+  
+  const family = {
+    children: ['Maria', 'João'],
+    wife: 'Ana',
+  };
+  
+  Object.assign(person, info, family)
+  console.log(person)
+  
+  /* Output
+    { name: 'Alberto',
+    lastName: 'Gomes',
+    age: 23,
+    job: 'engenheiro',
+    children: [ 'Maria', 'João' ],
+    wife: 'Ana'
+    } */
+
+    const person = {
+        name: 'Roberto',
+      };
+      
+      const lastName = {
+        lastName: 'Silva',
+      };
+      
+      const clone = Object.assign(person, lastName);
+      
+      console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+      console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+      clone.name = 'Maria';
+
+      //
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+//
+
+const obj = { value1: 10, value2: 11 };
+const cloneObj = obj;
+
+//
+
+const person = {
+    name:'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const newPerson = Object.assign({},person,lastName);
+  newPerson.name = 'Gilberto';
+  console.log(newPerson);
+  console.log(person);
